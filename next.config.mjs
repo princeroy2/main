a67fd3cf56b35d63f5500ca21d35e73954acf39a
unknown <abudlrouf8010@gmail.com>
2025-03-06 01:13:31 +0500

    new chang

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  server: {
+    timeout: 60000, // Set timeout to 60 seconds (default is 30 seconds)
+  },
     images: {
       domains: [
         'cnews24.ru', 
